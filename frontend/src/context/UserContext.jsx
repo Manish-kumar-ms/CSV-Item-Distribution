d931a414f457f7cb05414cd90ab420a42c706eee
Manish Kumar <ms5798034@gmail.com>
2025-09-21 00:47:54 +0530

    Update UserContext.jsx

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -5,7 +5,7 @@ import axios from 'axios';
 export const UserDataContext = createContext();
 
 const UserContext = ({ children }) => {
-   const serverUrl = "http://localhost:8000";
+   const serverUrl = "https://ai-powered-docs-hub-backend.onrender.com";
  
   const [userData, setUserData] = useState(null);
   const [loading, setLoading] = useState(true); // 🔁 Add loading flag
